91bd5b1cf299e18348cb8c57d7e3cb639c735fbd
avishek04 <avishek04c@gmail.com>
2021-11-04 12:41:56 +0530

    Adding marioLess file

---
+++ marioLess.c
@@ -0,0 +1,36 @@
+//
+//  marioLess.c
+//  
+//
+//  Created by Avishek Choudhury on 04/11/21.
+//
+#include <cs50.h>
+#include <stdio.h>
+
+int get_number(string num)
+{
+    int a;
+    do
+    {
+        a = get_int("%s", num);
+    }
+    while(a<1 || a>8);
+    return a;
+}
+
+int main(void)
+{
+    int n=get_number("Height: ");
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=0; j<n-i; j++)
+        {
+            printf(" ");
+        }
+        for(int k=0; k<i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
